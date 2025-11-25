fc10ac5d0c801beb30590a56eb1b061dfebc3738
Antti Mikkonen <antti.mikkonen@fmi.fi>
2025-11-25 17:35:31 +0200

    e2 almost ready

--- intercomparison_runs.py
+++ intercomparison_runs.py
@@ -42,7 +42,24 @@ if casesel == 'd1':
                 'wavelength' : 450,
                 'albedo' : 0.0,
                 'n_lay' : 1,                
-                'rayleigh_depol' : 0.03}
+                'rayleigh_depol' : 0.03} # currently hardcoded into Siro
+elif casesel == 'e1':
+    alttau = np.genfromtxt('datafiles/atmos/tau_rayleigh_450nm_usstd.dat',comments='#')
+    # nasty little layerses...
+    # internally Siro linearly interpolates between levels, so more than one homogenous
+    # layer is a pain to set up, but let's do it
+    smaller_alts = alttau.copy()
+    smaller_alts[:,0] = smaller_alts[:,0] - 1e-4 # 10 cm lower
+    new_alttau = np.concatenate((alttau,smaller_alts))
+    alttau = new_alttau[new_alttau[:,0].argsort()]
+    alttau[:,1] = np.roll(alttau[:,1],1)
+    alttau[-1,1] = alttau[-2,1]
+    alttau = alttau[1:,:]
+    settings = {'n_sca' : 1,
+                'n_abs' : 1, # needs to be one, but filled with zeroes
+                'alttau' : alttau,
+                'wavelength' : 450,
+                'albedo' : 0.0}
 elif casesel == 'd3':
     siro_custom_settings['AER_FILENAME'] = "'input/miefiles/%s'"
     
@@ -59,6 +76,11 @@ def save_results_to_nc(casesel,idx_sza,stokes):
     with netCDF4.Dataset(outfilename,'a') as ds:
         ds[var_names[-1]][:,idx_sza,0,:,:,:] = stokes_in
         
+def tau_to_xsec(tau,thickness):
+    # converts optical thicknesses in km to xsec in cm2
+    # assuming the number density to be 1/cm3
+    return tau / thickness / 1e5
+    
 for idx_sza in range(len(szas)):
     #for idx_sza in [0]:    
     compute_siro = True
@@ -72,8 +94,11 @@ for idx_sza in range(len(szas)):
     wl = np.array([settings['wavelength']])
     
     n_wl = n_wn
-    n_lev = settings['n_lay'] + 1
-    altitudes = np.linspace(0,atmos_thickness,n_lev)
+    if casesel == 'd1':
+        n_lev = settings['n_lay'] + 1
+        altitudes = np.linspace(0,atmos_thickness,n_lev)
+    elif casesel == 'e1':
+        altitudes = settings['alttau'][:,0]
     
     n_medium_positions = altitudes.size
     n_coordinate = 3
@@ -101,7 +126,7 @@ for idx_sza in range(len(szas)):
         # single layer Rayleigh
         
         medium['scatterer'][:,0] = 1.0 
-        medium['scattering_cross_section'] = settings['tau_sca'] / atmos_thickness / 1e5
+        medium['scattering_cross_section'] = tau_to_xsec(settings['tau_sca'],atmos_thickness)
         
         # zero stands for rayleigh-scattering
         medium['scatterer_kernel'] = np.zeros((n_scatterer,))
@@ -111,7 +136,15 @@ for idx_sza in range(len(szas)):
         
     elif casesel == 'd3':
         medium['scatterer_kernel'][1] = 1 # this is to use the aerosols
-    
+    elif casesel == 'e1':
+        alttau = settings['alttau']
+        medium['scatterer'][:,0] = 1.0 
+        thicknesses = np.diff(alttau[::2,0])
+        for i in range(0,99,2):
+            thick_idx = i//2
+            medium['scattering_cross_section'][i,0,0] = tau_to_xsec(alttau[i,1],thicknesses[thick_idx])
+            medium['scattering_cross_section'][i+1,0,0] = medium['scattering_cross_section'][i,0,0]
+    halt
     #emitters and emissivities
     medium['emitter'] = np.zeros((n_medium_positions,n_emitter))
     medium['medium_emissivity'] = np.zeros((n_medium_positions,n_wl,4,n_emitter))
