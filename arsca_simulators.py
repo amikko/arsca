19e82fe424d4236b0001f0259b456ee11d5427cd
Antti Mikkonen <antti.mikkonen@fmi.fi>
2025-11-25 13:10:28 +0200

    make default aerosol case more sensible for siro settings

--- arsca_simulators.py
+++ arsca_simulators.py
@@ -223,7 +223,7 @@ def create_siro_settings(custom_settings={}):
                 'step' : 2.0,
                 'brdf_reflection' : False,
                 'BRF_FILENAME' : "'input/brdf/new_snow.nc4'",
-                'AER_FILENAME' : "'input/miefiles/aer_ALTIUS.dat'"}
+                'AER_FILENAME' : "'input/miefiles/aer_WCO2.dat'"}
 
     #replace the default settings with new values if desired
     default_settings_keys = list(settings.keys())
